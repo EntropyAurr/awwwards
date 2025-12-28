a7dbfdae3585fea85fe11b25275e45fda34bd014
Aurora <auduong.301@gmail.com>
2025-12-28 18:01:43 +0700

    create hero section

---
+++ src/components/Button.jsx
@@ -0,0 +1,15 @@
+function Button({ id, title, leftIcon, rightIcon, containerClass }) {
+  return (
+    <button id={id} className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-teal-50 px-7 py-3 text-black ${containerClass}`}>
+      {leftIcon}
+
+      <span className="font-general relative inline-flex overflow-hidden text-xs uppercase">
+        <div>{title}</div>
+      </span>
+
+      {rightIcon}
+    </button>
+  );
+}
+
+export default Button;
